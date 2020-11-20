0ae707f42d4a2459db5a82d449a4b5c4099133e7
Abunaw Ose <abunaw.ose@careerbuilder.com>
2020-11-20 12:50:37 -0500

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #assets compile on heroku
+  config.assets.compile = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
