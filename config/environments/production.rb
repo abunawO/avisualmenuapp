7318ea4bf592cc93760897ecde18fdfd2550388b
Abunaw Ose <abunaw.ose@careerbuilder.com>
2020-11-23 11:19:08 -0500

    WIP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'apikey',
+    :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'avisualmenuapp.com',
     :enable_starttls_auto => true
